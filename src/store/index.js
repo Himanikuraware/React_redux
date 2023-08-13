36df4de0b8576ca30042d76708f109f9bfa1f0b1
Himanikuraware <kurawarehimani26@gmail.com>
2023-08-13 14:28:07 +0530

    Attached Payload to the actions.

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,16 @@
 // createstore is deprecated that's why we have to import legacy_createStore instead of createStore.
-import { legacy_createStore as createStore  } from "redux";
+import { legacy_createStore as createStore } from "redux";
 
 const counterReducer = (state = { counter: 0 }, action) => {
   if (action.type === "increment") {
     return {
       counter: state.counter + 1,
+    };
+  }
+
+  if (action.type === "increase") {
+    return {
+      counter: state.counter + action.value,
     };
   }
 
